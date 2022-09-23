fa5568ec722640e96e2c1ea791c0eb8e89218b90
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2022-09-23 08:27:23 +0100

    requetes pour les colleges

--- filtrage_college.sql
+++ filtrage_college.sql
@@ -8,10 +8,30 @@ SELECT college.name FROM college, adresse
 WHERE college.id_adresse = adresse.id 
 AND adresse.pays = 'Maroc'
 
+-- Departement par college 
+SELECT departement.name FROM departement, college
+WHERE departement.id_college = college.numero AND college.numero = 5
+
+
 -- nombre des etudiant par college
 SELECT  COUNT(DISTINCT etudiant.id_user) FROM etudiant, evaluation, matiere, enseignant, departement, college
 WHERE etudiant.id_user = evaluation.id_etudiant 
 AND evaluation.id_matiere = matiere.id AND enseignant.id_matiere = matiere.id
 AND enseignant.id_departement = departement.id AND departement.id_college = 3
 
+-- Nombe d'étudiant par college
+SELECT COUNT(DISTINCT etudiant.id_user) FROM etudiant, enseigner, enseignant, departement, college
+WHERE etudiant.id_user = enseigner.id_etudiant AND enseigner.id_enseignant = enseignant.id_user
+AND enseignant.id_departement = departement.id AND departement.id_college = 5
+
 -- moyenne par college 
+SELECT AVG(evaluation.note) FROM etudiant, enseigner, enseignant, departement, college, evaluation
+WHERE etudiant.id_user = evaluation.id_etudiant AND etudiant.id_user = enseigner.id_etudiant AND enseigner.id_enseignant = enseignant.id_user
+AND enseignant.id_departement = departement.id AND departement.id_college = 5
+
+
+-- les enseignants par college 
+SELECT DISTINCT users.firstname, users.lastname FROM users, enseignant, departement, college
+WHERE college.numero = 4 AND departement.id_college = college.numero AND 
+enseignant.id_departement = departement.id
+
