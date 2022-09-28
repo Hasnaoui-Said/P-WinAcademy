60e22a3ad50b4921853c2af3ce57968f81d8f011
said hasnaoui <saidhasnaoui.uce@gmail.com>
2022-09-28 17:21:35 +0100

    update file name

--- filtrage-matiere(said).sql
+++ filtrage-matiere(said).sql
@@ -41,3 +41,18 @@ WHERE etudiant.id_user = 36;
 SELECT matiere.name, SUM(note)/count(matiere.name) as `moyen` FROM matiere 
 INNER JOIN evaluation ON evaluation.id_matiere = matiere.id
 GROUP BY matiere.name;
+
+SELECT departement.name as `deparement name`, users.*, role.name as `role` FROM `departement` 
+INNER JOIN responsable on departement.id = responsable.id_departement
+INNER JOIN user_role ON user_role.id_user = responsable.id_enseignant
+INNER JOIN role ON role.id = user_role.id_role
+INNER JOIN users ON users.id = responsable.id_enseignant
+
+
+SELECT *, AVG(evaluation.note) AS `moyen of matiere` FROM `evaluation` 
+INNER JOIN matiere on matiere.id = evaluation.id_matiere
+INNER JOIN etudiant ON etudiant.id_user = evaluation.id_etudiant
+INNER  JOIN users ON users.id = evaluation.id_etudiant
+INNER JOIN user_role ON user_role.id_user = etudiant.id_user
+INNER JOIN role ON role.id = user_role.id_role
+group by evaluation.id_etudiant, evaluation.id_matiere
